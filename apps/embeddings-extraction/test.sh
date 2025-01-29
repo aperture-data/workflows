03b5778fcddcdae97f7c557f274488db30856221
luisremis <luis@aperturedata.io>
2025-01-29 12:35:39 -0300

    Add embeddings-extracion app (#3)

--- apps/embeddings-extraction/test.sh
+++ apps/embeddings-extraction/test.sh
@@ -5,14 +5,14 @@ bash build.sh
 
 docker stop $(docker ps -q)  || true
 docker rm $(docker ps -a -q) || true
-docker network rm object-detection || true
+docker network rm embeddings-extraction || true
 
-docker network create object-detection
+docker network create embeddings-extraction
 
 # Start empty aperturedb instance
 docker run -d \
            --name aperturedb \
-           --network object-detection \
+           --network embeddings-extraction \
            -p 55555:55555 \
            -e ADB_MASTER_KEY="admin" \
            -e ADB_KVGD_DB_SIZE="204800" \
@@ -22,7 +22,7 @@ sleep 20
 
 # Add images to the db
 docker run --name add_image \
-           --network object-detection \
+           --network embeddings-extraction \
            -e TOTAL_IMAGES=100 \
            -e DB_HOST=aperturedb \
            -v ./input:/app/data \
@@ -30,14 +30,14 @@ docker run --name add_image \
 
 # Run the object detection workflow
 docker run \
-           --network object-detection \
+           --network embeddings-extraction \
            -e DB_HOST=aperturedb \
            -e RUN_ONCE=true \
-           -e MODEL_NAME="frcnn-mobilenet" \
-           aperturedata/workflows-object-detection
+           -e MODEL_NAME="ViT-B/16" \
+           aperturedata/workflows-embeddings-extraction
 
 # if CLEANUP is set to true, stop the aperturedb instance and remove the network
 if [ "$CLEANUP" = "true" ]; then
     docker stop aperturedb
-    docker network rm object-detection
+    docker network rm embeddings-extraction
 fi
