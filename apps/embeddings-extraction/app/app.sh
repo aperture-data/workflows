ad8295677d86316d2dd2d84db12b35f59d8ce7a8
Luis Remis <luis@aperturedata.io>
2025-02-06 20:50:15 +0000

    automatic commit message

--- apps/embeddings-extraction/app/app.sh
+++ apps/embeddings-extraction/app/app.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
 set -e
 
-python3 extract_embeddings.py
+# Only return upon error
+while true; do
+    python3 extract_embeddings.py
+
+    if [ "$RUN_ONCE" = "true" ]; then
+        break
+    fi
+    sleep 30
+done
