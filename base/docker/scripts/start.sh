3244147453f961853d9aeaa5fb0ec8c267ac00eb
luisremis <luis@aperturedata.io>
2025-02-18 00:25:38 -0300

    Update envars for workflows (#16)

--- base/docker/scripts/start.sh
+++ base/docker/scripts/start.sh
@@ -13,46 +13,10 @@ APPLOG="${OUTPUT}/app.log"
 S3LOGFILE="upload_s3.log"
 
 
-if [ -z "${AWS_ACCESS_KEY_ID}" ]; then
-    AWS_ACCESS_KEY_ID=$(jq -r .access_key <<< ${WF_LOGS_AWS_CREDENTIALS})
-fi
-if [ -z "${AWS_SECRET_ACCESS_KEY}" ]; then
-    AWS_SECRET_ACCESS_KEY=$(jq -r .secret_key <<< ${WF_LOGS_AWS_CREDENTIALS})
-fi
+ENVIRONMENT=${ENVIRONMENT:="develop"}
 
 if [ -z "${KEEP_PREV_OUTPUT}" ]; then
     KEEP_PREV_OUTPUT=false
-fi
-
-if [ -z "${USERLOG_MSG}" ]; then
-    USERLOG_MSG=true
-fi
-
-if [ -z "${POST_TO_SLACK}" ]; then
-    POST_TO_SLACK=false
-fi
-
-if [ -z "${SLACK_CHANNEL}" ]; then
-    SLACK_CHANNEL="cronjobs"
-fi
-
-if [ -z "${SLACK_CHANNEL_FAILED}" ]; then
-    SLACK_CHANNEL_FAILED="cronjobs"
-fi
-
-if [ -z "${SLACK_BOT_TOKEN}" ]; then
-    SLACK_BOT_TOKEN=""
-    POST_TO_SLACK=false
-fi
-
-if [ -z "${PUSH_TO_S3}" ]; then
-    PUSH_TO_S3=true
-    if [ -z "${AWS_ACCESS_KEY_ID}" ]; then
-        PUSH_TO_S3=false
-    fi
-    if [ -z "${AWS_SECRET_ACCESS_KEY}" ]; then
-        PUSH_TO_S3=false
-    fi
 fi
 
 if [ "$KEEP_PREV_OUTPUT" = true ]; then
@@ -93,8 +57,6 @@ if [[ $USE_REST == true ]]; then
 fi
 
 DB_PORT=${DB_PORT:=$DEFAULT_DB_PORT}
-
-ENVIRONMENT=${ENVIRONMENT:="develop"}
 
 params=()
 if [[ $USE_SSL == false ]]; then
@@ -142,6 +104,8 @@ runtime=$((end-start))
 
 GRAFANA_END_TIME=$(($(date '+%s') * 1000))
 
+GRAFANA_URL="https://${DB_HOST}/grafana/d/mPHHiqbnk/aperturedb-connectivity-status?from=${GRAFANA_START_TIME}&to=${GRAFANA_END_TIME}&refresh=5s"
+
 adb utils execute summary >> db_summary_after.log
 diff db_summary_before.log db_summary_after.log >> db_summary_diff.log
 
@@ -159,8 +123,14 @@ fi
 DATE=$(date '+%Y-%m-%d') # Get current date
 SECONDS=$(date '+%s')    # seconds since epoch
 
+# UPLOAD RESULTS TO S3
+
 if [ "$PUSH_TO_S3" = true ]; then
 
+    if ([ -z "${AWS_ACCESS_KEY_ID}" ] || [ -z "${AWS_SECRET_ACCESS_KEY}" ]) && [ -n "${WF_LOGS_AWS_CREDENTIALS}" ]; then
+        AWS_ACCESS_KEY_ID=$(jq -r .access_key <<< ${WF_LOGS_AWS_CREDENTIALS})
+        AWS_SECRET_ACCESS_KEY=$(jq -r .secret_key <<< ${WF_LOGS_AWS_CREDENTIALS})
+    fi
     aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
     aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
     aws configure set default.region us-west-2
@@ -168,11 +138,11 @@ if [ "$PUSH_TO_S3" = true ]; then
     declare -A domains=([develop]=aperturedata.dev [main]=aperturedata.io);
 
     FILE_STASH_DOMAIN=${FILE_STASH_DOMAIN:=${domains[$ENVIRONMENT]}}
-    LOGS_BUCKET=${LOGS_BUCKET:="aperturedata-${ENVIRONMENT}-iris-workflows-logs"}
+    LOGS_BUCKET=${WF_LOGS_AWS_BUCKET:="aperturedata-${ENVIRONMENT}-iris-workflows-logs"}
 
     SUFFIX=$DATE/${APP_NAME}/${SECONDS}_${RUN_NAME}/
 
-    BUCKET=s3://$WF_LOGS_AWS_BUCKET/$SUFFIX
+    BUCKET=s3://$LOGS_BUCKET/$SUFFIX
 
     echo "Uploading results to S3..." >> $LOGFILE
     aws s3 sync --quiet ${OUTPUT}/ $BUCKET > $S3LOGFILE
@@ -182,16 +152,11 @@ if [ "$PUSH_TO_S3" = true ]; then
 
     URL=https://workflows-logs.${FILE_STASH_DOMAIN}/files/$LOGS_BUCKET/$SUFFIX
 
-    GRAFANA_URL="https://${DB_HOST}/grafana/d/mPHHiqbnk/aperturedb-connectivity-status?from=${GRAFANA_START_TIME}&to=${GRAFANA_END_TIME}&var-job_filter=job%7C%3D%7Caperturedb&var-pod_ip=All&var-node_filter=pod_ip%7C%3D~%7C$pod_ip&orgId=1&refresh=5s"
-
     echo "Results available at: $URL"
     echo "Dashboard: $GRAFANA_URL"
 
     echo "Results available at: $URL" >> $LOGFILE
     echo "Dashboard: $GRAFANA_URL" >> $LOGFILE
-
-    RESULTS="<$URL|Results>"
-    GRAFANA="<$GRAFANA_URL|Dashboards>"
 
     # Need to upload log.log again :)
     aws s3 cp $LOGFILE ${BUCKET}log.log >> $S3LOGFILE
@@ -200,6 +165,27 @@ else
     echo "=================== APP LOG ==================="
     cat $APPLOG
     echo "================= END APP LOG ================="
+fi
+
+# POST TO SLACK
+
+if [ "$POST_TO_SLACK" = true ]; then
+
+    SLACK_CHANNEL=${SLACK_CHANNEL:="cronjobs"}
+    SLACK_CHANNEL_FAILED=${SLACK_CHANNEL_FAILED:="alerts-${ENVIRONMENT}"}
+
+    RESULTS="<$URL|Results>" # URL may not be available if PUSH_TO_S3 is false
+    GRAFANA="<$GRAFANA_URL|Dashboards>"
+
+    if [ "${ret_val}" -ne 0 ]; then
+        python3 slack-alert.py \
+            -msg ":fire: App \`${APP_NAME}\` failed(\"${ret_val}\"). $RESULTS. $GRAFANA." \
+            -channel "${SLACK_CHANNEL_FAILED}"
+    else
+        python3 slack-alert.py \
+            -msg "App \`${APP_NAME}\` done. Took ${runtime}s. $RESULTS. $GRAFANA." \
+            -channel "${SLACK_CHANNEL}"
+    fi
 fi
 
 if [ "${ret_val}" -ne 0 ]; then
