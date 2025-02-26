3bda2b48993297a9c95e2348d4bb662be23e08f7
Gautam Saluja <52312085+gsaluja9@users.noreply.github.com>
2025-02-26 11:08:03 -0500

    copy hello.ipynb to /notebooks at startup, if not found (#24)

--- apps/jupyterlab/app/app.sh
+++ apps/jupyterlab/app/app.sh
@@ -13,3 +13,8 @@ configure_jupyter() {
 if [ ! -f /opt/.jupyter_configured ]; then
     configure_jupyter
 fi
+
+if [ ! -f "/notebooks/hello.ipynb" ]; then
+    echo "Creating a sample notebook..."
+    cp /aperturedata/hello.ipynb /notebooks/hello.ipynb
+fi
