3ca6c01eb22bbb517f5c08a3d7faed5bc2fc2733
Gautam Saluja <52312085+gsaluja9@users.noreply.github.com>
2025-02-28 11:42:40 -0500

    Resize ingestion image (#34)

--- base/docker/build.sh
+++ base/docker/build.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-
 set -e
 
 IMG_NAME="aperturedata/workflows-base"
 
 # Build docker image
-docker pull ${IMG_NAME}
-docker build --cache-from=${IMG_NAME} -t ${IMG_NAME} .
+docker build --no-cache -t ${IMG_NAME} .
+
+
