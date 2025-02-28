0f0f71d5908e8871b944fae863bc8c3be52e8e7c
Gautam <gautam@aperturedata.io>
2025-02-28 12:20:25 -0500

    passing generate embeddings.

--- apps/face-detection/app/app.sh
+++ apps/face-detection/app/app.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 set -e
-
+GENERATE_EMBEDDINGS=${GENERATE_EMBEDDINGS:-false}
 
 SLEEPING_TIME=${SLEEPING_TIME:-30}
 
 # Only return upon error
 while true; do
-    python3 face_detection.py
+    python3 face_detection.py -generate_embeddings $GENERATE_EMBEDDINGS
 
     if [ "$RUN_ONCE" = "true" ]; then
         break
