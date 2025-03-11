0403cfa9232b45c8ae30d9c0ab27cd0d0f7a89be
Luis Remis <luis@aperturedata.io>
2025-03-11 16:07:42 +0000

    automatic commit message

--- apps/create_new_workflow.sh
+++ apps/create_new_workflow.sh
@@ -11,8 +11,7 @@ if [ -z "$NAME" ]
 fi
 
 cp -r example $NAME
-sed -i -e 's/workflows-example/workflows-${NAME}/g' $NAME/Dockerfile
-sed -i -e 's/workflows-example/workflows-${NAME}/g' $NAME/build.sh
+sed -i -e "s/workflows-example/workflows-${NAME}/g" $NAME/Dockerfile
 
 echo "Workflow $NAME created."
 echo "Suggestion: create a commit now to track your changes on the newly created workflow"
