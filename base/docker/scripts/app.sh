71cc0dbad4bd2e4adf9d90e2aa25a7e0b9837b51
luisremis <luis@aperturedata.io>
2025-07-08 11:13:35 -0300

    Add mock status server for dev/testing (#80)

--- base/docker/scripts/app.sh
+++ base/docker/scripts/app.sh
@@ -1,4 +1,16 @@
 
 echo "This is the app.sh file from aperturedata/workflows-base"
 echo "You have to implement me. You did not."
+
+# Set a default value for STAY_ALIVE if it is currently unset or null.
+: "${STAY_ALIVE:=false}"
+
+if [ "${STAY_ALIVE}" = "true" ]; then
+    echo "STAY_ALIVE is set to true. I will not exit."
+    # This loop will run indefinitely.
+    while true; do
+        sleep 60
+    done
+fi
+
 echo "I did nothing. Bye."
