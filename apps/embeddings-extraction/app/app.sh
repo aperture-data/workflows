0c3cececb7bbe55d6f041d2aae6bca13d94b3bef
bovlb <31326650+bovlb@users.noreply.github.com>
2025-07-24 16:44:07 -0700

    text search in notebook; segment filtering (#107)
    * checkpoint
    
    * reduce logging
    
    * improve segment filtering
    
    * Make RUN_ONCE case insensitive
    
    * Add segment_number
    
    * tweak text
    
    * use static methods

--- apps/embeddings-extraction/app/app.sh
+++ apps/embeddings-extraction/app/app.sh
@@ -8,7 +8,7 @@ while true; do
     python3 status.py --completed 0 --phases processing --phases sleeping --phase processing
     python3 log_processor.py 'python3 extract_embeddings.py'
 
-    if [ "$RUN_ONCE" = "true" ]; then
+    if [ "${RUN_ONCE,,}" = "true" ]; then
         break
     fi
     python3 status.py --completed 0 --phases processing --phases sleeping --phase sleeping
