08ff6e292b103ce5c2c504525c8a8b8636e82ef4
Gavin Matthews <gavin@aperturedata.io>
2025-07-29 03:48:35 +0000

    seems to be working

--- apps/postgresql-server/fdw/fdw/__init__.py
+++ apps/postgresql-server/fdw/fdw/__init__.py
@@ -1,25 +1,6 @@
+from multicorn import TableDefinition, ColumnDefinition, ForeignDataWrapper
 import sys
 from datetime import datetime
-sys.path.insert(0, "/opt/venv/lib/python3.10/site-packages")
-
-with open("/tmp/fdw_hook.log", "a") as f:
-    # timestamp the log entry
-    f.write(f"{datetime.now().isoformat()} - ")
-    f.write("FDW hook loaded by: " + " ".join(sys.argv) + "\n")
-
-    f.write("\n".join(sys.path))
-
-with open("/tmp/fdw_hook.log", "a") as f:
-    try:
-        from multicorn import ForeignDataWrapper
-        f.write("Successfully imported ForeignDataWrapper\n")
-        f.flush()
-    except Exception as e:
-        import traceback
-        f.write("FAILED to import multicorn:\n")
-        traceback.print_exc(file=f)
-        f.flush()
-        raise
 
 
 class FDW(ForeignDataWrapper):
@@ -27,16 +8,17 @@ class FDW(ForeignDataWrapper):
         print("Executing FDW with quals:", quals, "and columns:", columns)
         return []
 
-    def import_schema(self, schema, srv_options, options, restriction_type,
-                      restricts):
-        print(f"Importing schema: {schema} with server options: {srv_options}, options: {options}, "
-              f"restriction_type: {restriction_type}, restricts: {restricts}")
+    @classmethod
+    def import_schema(cls, schema, srv_options, options, restriction_type, restricts):
         return [
-            {
-                "table_name": "demo_table",
-                "columns": [("id", "integer"), ("name", "text")],
-                "options": {"dummy_option": "value"}
-            }
+            TableDefinition(
+                "my_table",
+                columns=[
+                    ColumnDefinition(column_name="id", type_name="integer"),
+                    ColumnDefinition(column_name="name", type_name="text"),
+                ],
+                options={"some_option": "value"}
+            )
         ]
 
 
