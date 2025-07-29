61dfc9712fb87286aff9579f9744eb49e249f583
Gavin Matthews <gavin@aperturedata.io>
2025-07-29 22:45:46 +0000

    remove files no longer needed

--- apps/postgresql-server/sitecustomize.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.path.insert(0, "/opt/venv/lib/python3.10/site-packages")
-sys.path.insert(0, "/app")
-
-with open("/tmp/sitecustomize.log", "a") as f:
-    f.write("sitecustomize loaded by: " + " ".join(sys.argv) + "\n")
-    f.write("\n".join(sys.path) + "\n")
