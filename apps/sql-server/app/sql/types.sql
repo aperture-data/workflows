9a0191dfbe1fe21ed327064dd34bf1e9360ab475
Gavin Matthews <gavin@aperturedata.io>
2025-07-31 23:15:56 +0000

    Add some enums for videos

--- apps/sql-server/app/sql/types.sql
+++ apps/sql-server/app/sql/types.sql
@@ -1,3 +1,5 @@
 -- Enums
 
 CREATE TYPE image_format_enum AS ENUM ('png', 'jpg');
+CREATE TYPE video_codec_enum AS ENUM ('xvid', 'h263', 'h264');
+CREATE TYPE video_container_enum AS ENUM('mp4', 'avi', 'mov');
