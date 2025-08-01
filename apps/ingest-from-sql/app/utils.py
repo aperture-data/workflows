1f838b95080edff522562b442b8290a8a6970a18
Drew Ogle <104358188+drewaogle@users.noreply.github.com>
2025-07-31 21:53:51 -0400

    WF 6:  handle cloud input  (#118)
    * allow empty strings in arg parsing for list and map
    
    * Apply suggestions from code review
    
    Co-authored-by: bovlb <31326650+bovlb@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: bovlb <31326650+bovlb@users.noreply.github.com>

--- apps/ingest-from-sql/app/utils.py
+++ apps/ingest-from-sql/app/utils.py
@@ -47,9 +47,12 @@ class CommandlineType:
     def column_list(input_str):
         items = input_str.split(',')
         for item in items:
+            item = item.strip()
+            if item == "":
+                continue
             parts = item.split('.')
             if len(parts) != 2:
-                raise Exception(f"Column list item {item} does not have 2 parts; expect table.column")
+                raise ValueError(f"Column list item \"{item}\" does not have 2 parts; expect table.column")
         return items
 
     @staticmethod
@@ -57,9 +60,12 @@ class CommandlineType:
         output = {}
         items = input_str.split(',')
         for item in items:
+            item = item.strip()
+            if item == "":
+                continue
             pair = item.split(':')
             if len(pair) != 2:
-                raise Exception(f"Map pair item {item} does not have 2 parts; expect key:value")
+                raise ValueError(f"Map pair item {item} does not have 2 parts; expect key:value")
             k,v = pair
             k = k.strip()
             if k in output:
