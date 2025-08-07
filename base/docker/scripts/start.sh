dcf9aa3922d3c44ec350d3aa71d75ed5b6c2b241
Gautam <gautam@aperturedata.io>
2025-08-07 10:31:54 -0400

    Bail out on connectivity error.

--- base/docker/scripts/start.sh
+++ base/docker/scripts/start.sh
@@ -94,126 +94,137 @@ adb config create default \
 
 echo "Verifying connectivity to ${DB_HOST}..."
 adb utils execute status >> $LOGFILE
-echo "Done."
+connectivity_status=$?
 
-adb utils execute summary >> db_summary_before.log
-
-if [ "$USERLOG_MSG" == true ]; then
-    python3 userlog.py --info "Starting App ${APP_NAME} - Run: ${RUN_NAME}."
-fi
-
-GRAFANA_START_TIME=$(($(date '+%s') * 1000))
-
-
-
-# Move all log files to output folder
-
-start=`date +%s`
-echo "Starting App: ${APP_NAME} - Run: ${RUN_NAME}..."
-echo "Starting App: ${APP_NAME} - Run: ${RUN_NAME}..." >> $LOGFILE
-
-bash app.sh |& tee -a $APPLOG
-ret_val="${PIPESTATUS[0]}"
-
-if [ "${ret_val}" -ne 0 ]; then
-    python status.py --completed 0 --error-message "failed" --error-code "workflow_error"
-fi
-
-
-echo "App Done." >> $LOGFILE
-
-echo "App Done."
-end=`date +%s`
-runtime=$((end-start))
-
-GRAFANA_END_TIME=$(($(date '+%s') * 1000))
-
-GRAFANA_URL="https://${DB_HOST_PUBLIC}/grafana/d/mPHHiqbnk/aperturedb-connectivity-status?from=${GRAFANA_START_TIME}&to=${GRAFANA_END_TIME}&refresh=5s"
-
-adb utils execute summary >> db_summary_after.log
-diff db_summary_before.log db_summary_after.log >> db_summary_diff.log
-
-if [ "$USERLOG_MSG" == true ]; then
-    python3 userlog.py --info "Done App ${APP_NAME} - Run: ${RUN_NAME}."
-fi
-
-date >> $LOGFILE
-
-# Move all log files to output folder
-if compgen -G "*.log" > /dev/null; then
-    mv *.log ${OUTPUT}/
-fi
-
-DATE=$(date '+%Y-%m-%d') # Get current date
-SECONDS=$(date '+%s')    # seconds since epoch
-
-# UPLOAD RESULTS TO S3
-
-if [ "$PUSH_TO_S3" == true ]; then
-
-    if ([ -z "${AWS_ACCESS_KEY_ID}" ] || [ -z "${AWS_SECRET_ACCESS_KEY}" ]) && [ -n "${WF_LOGS_AWS_CREDENTIALS}" ]; then
-        AWS_ACCESS_KEY_ID=$(jq -r .access_key <<< ${WF_LOGS_AWS_CREDENTIALS})
-        AWS_SECRET_ACCESS_KEY=$(jq -r .secret_key <<< ${WF_LOGS_AWS_CREDENTIALS})
-    fi
-    aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
-    aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
-    aws configure set default.region us-west-2
-
-    declare -A domains=([develop]=aperturedata.dev [main]=aperturedata.io);
-
-    FILE_STASH_DOMAIN=${FILE_STASH_DOMAIN:-${domains[$ENVIRONMENT]}}
-    LOGS_BUCKET=${WF_LOGS_AWS_BUCKET:-"aperturedata-${ENVIRONMENT}-iris-workflows-logs"}
-
-    SUFFIX=$DATE/${APP_NAME}/${SECONDS}_${RUN_NAME}/
-
-    BUCKET=s3://$LOGS_BUCKET/$SUFFIX
-
-    echo "Uploading results to S3..." >> $LOGFILE
-    aws s3 sync --quiet ${OUTPUT}/ $BUCKET > $S3LOGFILE
-    echo "Done uploading results to S3." >> $LOGFILE
-    date >> $LOGFILE
-    echo "All done. Bye." >> $LOGFILE
-
-    URL=https://workflows-logs.${FILE_STASH_DOMAIN}/files/$LOGS_BUCKET/$SUFFIX
-
-    echo "Results available at: $URL"
-    echo "Dashboard: $GRAFANA_URL"
-
-    echo "Results available at: $URL" >> $LOGFILE
-    echo "Dashboard: $GRAFANA_URL" >> $LOGFILE
-
-    # Need to upload log.log again :)
-    aws s3 cp $LOGFILE ${BUCKET}log.log >> $S3LOGFILE
+if [ "${connectivity_status}" -ne 0 ]; then
+    python status.py --completed 0 --error-message "Could not connect to database" --error-code "workflow_error"
+    ret_val=${connectivity_status}
 else
-    echo "Not uploading results to S3."
-    echo "=================== APP LOG ==================="
-    cat $APPLOG
-    echo "================= END APP LOG ================="
-fi
+    echo "Done."
 
-# POST TO SLACK
+    adb utils execute summary >> db_summary_before.log
 
-if [ "$POST_TO_SLACK" == true ]; then
+    if [ "$USERLOG_MSG" == true ]; then
+        python3 userlog.py --info "Starting App ${APP_NAME} - Run: ${RUN_NAME}."
+    fi
 
-    SLACK_CHANNEL=${SLACK_CHANNEL:-"cronjobs"}
-    SLACK_CHANNEL_FAILED=${SLACK_CHANNEL_FAILED:-"alerts-${ENVIRONMENT}"}
+    GRAFANA_START_TIME=$(($(date '+%s') * 1000))
 
-    RESULTS="<$URL|Results>" # URL may not be available if PUSH_TO_S3 is false
-    GRAFANA="<$GRAFANA_URL|Dashboards>"
+
+
+    # Move all log files to output folder
+
+    start=`date +%s`
+    echo "Starting App: ${APP_NAME} - Run: ${RUN_NAME}..."
+    echo "Starting App: ${APP_NAME} - Run: ${RUN_NAME}..." >> $LOGFILE
+
+    bash app.sh |& tee -a $APPLOG
+    ret_val="${PIPESTATUS[0]}"
 
     if [ "${ret_val}" -ne 0 ]; then
-        python3 slack-alert.py \
-            -msg ":fire: App \`${APP_NAME}\` failed(\"${ret_val}\"). $RESULTS. $GRAFANA." \
-            -channel "${SLACK_CHANNEL_FAILED}"
-    else
-        python3 slack-alert.py \
-            -msg "App \`${APP_NAME}\` done. Took ${runtime}s. $RESULTS. $GRAFANA." \
-            -channel "${SLACK_CHANNEL}"
+        python status.py --completed 0 --error-message "failed" --error-code "workflow_error"
     fi
-fi
 
-if [ "${ret_val}" -ne 0 ]; then
-    echo "Error with app: $APP_NAME. Exiting with error code: ${ret_val}"
+
+    echo "App Done." >> $LOGFILE
+
+    echo "App Done."
+    end=`date +%s`
+    runtime=$((end-start))
+
+    GRAFANA_END_TIME=$(($(date '+%s') * 1000))
+
+    GRAFANA_URL="https://${DB_HOST_PUBLIC}/grafana/d/mPHHiqbnk/aperturedb-connectivity-status?from=${GRAFANA_START_TIME}&to=${GRAFANA_END_TIME}&refresh=5s"
+
+    adb utils execute summary >> db_summary_after.log
+    diff db_summary_before.log db_summary_after.log >> db_summary_diff.log
+
+    if [ "$USERLOG_MSG" == true ]; then
+        python3 userlog.py --info "Done App ${APP_NAME} - Run: ${RUN_NAME}."
+    fi
+
+    date >> $LOGFILE
+
+    # Move all log files to output folder
+    if compgen -G "*.log" > /dev/null; then
+        mv *.log ${OUTPUT}/
+    fi
+
+    DATE=$(date '+%Y-%m-%d') # Get current date
+    SECONDS=$(date '+%s')    # seconds since epoch
+
+    # UPLOAD RESULTS TO S3
+
+    if [ "$PUSH_TO_S3" == true ]; then
+
+        if ([ -z "${AWS_ACCESS_KEY_ID}" ] || [ -z "${AWS_SECRET_ACCESS_KEY}" ]) && [ -n "${WF_LOGS_AWS_CREDENTIALS}" ]; then
+            AWS_ACCESS_KEY_ID=$(jq -r .access_key <<< ${WF_LOGS_AWS_CREDENTIALS})
+            AWS_SECRET_ACCESS_KEY=$(jq -r .secret_key <<< ${WF_LOGS_AWS_CREDENTIALS})
+        fi
+        aws configure set aws_access_key_id $AWS_ACCESS_KEY_ID
+        aws configure set aws_secret_access_key $AWS_SECRET_ACCESS_KEY
+        aws configure set default.region us-west-2
+
+        declare -A domains=([develop]=aperturedata.dev [main]=aperturedata.io);
+
+        FILE_STASH_DOMAIN=${FILE_STASH_DOMAIN:-${domains[$ENVIRONMENT]}}
+        LOGS_BUCKET=${WF_LOGS_AWS_BUCKET:-"aperturedata-${ENVIRONMENT}-iris-workflows-logs"}
+
+        SUFFIX=$DATE/${APP_NAME}/${SECONDS}_${RUN_NAME}/
+
+        BUCKET=s3://$LOGS_BUCKET/$SUFFIX
+
+        echo "Uploading results to S3..." >> $LOGFILE
+        aws s3 sync --quiet ${OUTPUT}/ $BUCKET > $S3LOGFILE
+        echo "Done uploading results to S3." >> $LOGFILE
+        date >> $LOGFILE
+        echo "All done. Bye." >> $LOGFILE
+
+        URL=https://workflows-logs.${FILE_STASH_DOMAIN}/files/$LOGS_BUCKET/$SUFFIX
+
+        echo "Results available at: $URL"
+        echo "Dashboard: $GRAFANA_URL"
+
+        echo "Results available at: $URL" >> $LOGFILE
+        echo "Dashboard: $GRAFANA_URL" >> $LOGFILE
+
+        # Need to upload log.log again :)
+        aws s3 cp $LOGFILE ${BUCKET}log.log >> $S3LOGFILE
+    else
+        echo "Not uploading results to S3."
+        echo "=================== APP LOG ==================="
+        cat $APPLOG
+        echo "================= END APP LOG ================="
+    fi
+
+    # POST TO SLACK
+
+    if [ "$POST_TO_SLACK" == true ]; then
+
+        SLACK_CHANNEL=${SLACK_CHANNEL:-"cronjobs"}
+        SLACK_CHANNEL_FAILED=${SLACK_CHANNEL_FAILED:-"alerts-${ENVIRONMENT}"}
+
+        RESULTS="<$URL|Results>" # URL may not be available if PUSH_TO_S3 is false
+        GRAFANA="<$GRAFANA_URL|Dashboards>"
+
+        if [ "${ret_val}" -ne 0 ]; then
+            python3 slack-alert.py \
+                -msg ":fire: App \`${APP_NAME}\` failed(\"${ret_val}\"). $RESULTS. $GRAFANA." \
+                -channel "${SLACK_CHANNEL_FAILED}"
+        else
+            python3 slack-alert.py \
+                -msg "App \`${APP_NAME}\` done. Took ${runtime}s. $RESULTS. $GRAFANA." \
+                -channel "${SLACK_CHANNEL}"
+        fi
+    fi
+
+    if [ "${ret_val}" -ne 0 ]; then
+        echo "Error with app: $APP_NAME. Exiting with error code: ${ret_val}"
+    fi
+
+
+
+
 fi
 
 echo "Sleeping for 4 seconds to allow statuses to be reported..."
