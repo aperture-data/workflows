120328c0f5b9781c80c5e9845ad38d88d71a560b
Drew Ogle <104358188+drewaogle@users.noreply.github.com>
2025-08-29 17:15:25 -0400

    Tweaks and Optimizations for LS (#176)
    * need to add / to static ( it started to complain )
    
    * add logging configuration, set more variable for optimizing LS run
    
    * add options for project and storage config
    
    * pass all args in
    
    * don't set key twice
    
    * allow these to be unset if wanted

--- apps/label-studio/app/label_studio.py
+++ apps/label-studio/app/label_studio.py
@@ -12,6 +12,7 @@ from spec import WorkflowSpec
 
 import subprocess
 import re
+import json
 
 
 logger = logging.getLogger(__name__)
@@ -20,10 +21,10 @@ def main(args):
     db = create_connector()
 
     if args.delete:
-        WorkflowSpec.delete( "label-studio", args.spec_id )
+        WorkflowSpec.delete_spec(db, "label-studio", args.spec_id )
         sys.exit(0)
     elif args.delete_all:
-        WorkflowSpec.delete_all( "label-studio") 
+        WorkflowSpec.delete_all_data( db, "label-studio") 
         sys.exit(0)
     elif args.delete_all_ls_data:
         # note we don't verify host/database - a host could not be available,
@@ -33,10 +34,13 @@ def main(args):
 
 
 
-    def add_path_vars( env ):
+    def add_common_vars( env ):
 
+        env["LABEL_STUDIO_DEBUG"]="FALSE" 
+        env["LABEL_STUDIO_APERTUREDB_KEY"]=db.config.deflate()
+        env["LABEL_STUDIO_LOG_CONFIG_YAML"]="/app/workflows_logging.yaml"
         full_path = None
-        if os.environ['DB_HOST_PUBLIC']:
+        if "DB_HOST_PUBLIC" in os.environ:
             # generate path for cloud
             full_path = "https://{}/labelstudio".format(os.environ['DB_HOST_PUBLIC'])
             logger.error(f"Set url from DB_HOST_PUBLIC: {full_path}")
@@ -69,39 +73,54 @@ def main(args):
             logger.error(f"Path is {full_path} and {subpath}")
             
             env["LABEL_STUDIO_HOST"] = full_path
-            env["LABEL_STUDIO_STATIC_PATH"] = "{}/static".format( subpath )
+            env["LABEL_STUDIO_STATIC_PATH"] = "{}/static/".format( subpath )
             env["LABEL_STUDIO_URL_BASE"] = "{}".format( subpath )
+        if args.label_studio_default_storage_limit is not None:
+            env["LABEL_STUDIO_APERTUREDB_DEFAULT_LIMIT"] = str(args.label_studio_default_storage_limit)
+        env["LABEL_STUDIO_APERTUREDB_RO_PREDS"] = str(args.label_studio_storage_annotations_ro)
+        env["LABEL_STUDIO_APERTUREDB_DEFAULT_LOAD_PREDS"] = str(args.label_studio_default_import_annotations)
 
 
     logger.info("Preparing for Label Studio configuration.")
     # do config for LS
     cfg_env = os.environ.copy()
-    add_path_vars(cfg_env)
+    add_common_vars(cfg_env)
     cfg_env["LABEL_STUDIO_USERNAME"]=args.label_studio_user
     cfg_env["LABEL_STUDIO_PASSWORD"]=args.label_studio_password
     if args.label_studio_token:
         cfg_env["LABEL_STUDIO_USER_TOKEN"]=args.label_studio_token
+    if args.label_studio_default_project_name != '':
+        cfg_env["LABEL_STUDIO_CREATE_PROJ_TITLE"] = args.label_studio_default_project_name
+    if args.label_studio_default_storage_name != '':
+        cfg_env["LABEL_STUDIO_DEFAULT_CLOUD_STORAGE"]="aperturedb"
+        with open( "/app/cloud.json" ,"w") as fp:
+            storage_config = {
+                    'title': args.label_studio_default_storage_name
+                    }
+            json.dump(storage_config,fp)
+        cfg_env["LABEL_STUDIO_CLOUD_STORAGE_JSON_PATH"]="/app/cloud.json"
+    spec = WorkflowSpec( db, "label-studio", args.spec_id, clean=args.clean )
+
     ret = subprocess.run("bash /app/label_studio_init.sh", shell=True, env=cfg_env)
+    run_id = uuid4()
+    spec.add_run( run_id )
+
     if ret.returncode != 0:
         logger.error("Label Studio configuration failed.")
     else:
         logger.info("Label Studio configuration suceeded.")
-        spec = WorkflowSpec( db, "label-studio", args.spec_id, clean=args.clean )
 
-        run_id = uuid4()
-        spec.add_run( run_id )
 
         spec.update_run(str(run_id), {
             "wf_linked_types" : ["LS_annotation", "_BoundingBox"],
             "wf_creator": "label-studio" 
             })
         ls_env = os.environ.copy()
-        add_path_vars(ls_env)
+        add_common_vars(ls_env)
         ls_env["WORKFLOW_NAME"]="label-studio"
         ls_env["WORKFLOW_SPEC_ID"]=args.spec_id 
         ls_env["WORKFLOW_RUN_ID"]=str(run_id)
         ls_env["LABEL_STUDIO_CONFIGURED_STORAGE_BACKENDS"]="aperturedb gcs s3" 
-        ls_env["LABEL_STUDIO_APERTUREDB_KEY"]=db.config.deflate()
         logger.error(f" ENV FOR MAIN IS: {ls_env}")
 
         logger.info("Preparing to start Label Studio.")
@@ -116,16 +135,24 @@ def main(args):
 def get_args():
     obj = ArgumentParser(support_legacy_envars=True)
 
+    # configuration options
+    obj.add_argument("--label-studio-default-project-name",type=str,default="ApertureDB Labeling",
+            help="Name of a project to be created automatically (set to '' to disable")
+    obj.add_argument("--label-studio-default-storage-name",type=str,default="ApertureDB",
+            help="Name of the aperturedb storage to be created automatically (set to '' to disable")
+    obj.add_argument("--label-studio-default-storage-limit",type=int,default=None,
+            help="Value to be set as the default of images to be ingested on a sync in LS.") 
+    obj.add_argument("--label-studio-default-import-annotations",type=bool,default=True,
+            help="Whether to by default import Bboxes as annotations") 
+    obj.add_argument("--label-studio-storage-annotations-ro",type=bool,default=False,
+            help="If bbox annotations imported in LS are readonly ( default False )") 
     # hosting options
     obj.add_argument("--label-studio-url-path",type=str,default=None,
             help="Path to host label studio on other than /. Supply full external path: eg. http://localhost:8888/labelstudio ")
     # login options
-    obj.add_argument("--label-studio-token",type=str,default=None,
-            help="User token for label studio") 
-    obj.add_argument("--label-studio-user",type=str,default=None,required=True,
-            help="User for label studio") 
-    obj.add_argument("--label-studio-password",type=str,default=None,required=True,
-            help="User password for label studio") 
+    obj.add_argument("--label-studio-token",type=str,default=None, help="User token for label studio") 
+    obj.add_argument("--label-studio-user",type=str,default=None, help="User for label studio") 
+    obj.add_argument("--label-studio-password",type=str,default=None, help="User password for label studio") 
 
     # workflow options
     obj.add_argument("--spec-id",type=str,default=None,
@@ -150,6 +177,16 @@ def get_args():
 
     sanitized_params = {k: v if v is None or k not in to_sanitize else "**HIDDEN**" for k,v in params.__dict__.items()}
     logger.info(f"Parsed arguments: {sanitized_params}")
+
+    if not(params.delete or params.delete_all or params.delete_all_ls_data) and \
+            (params.label_studio_user is None or params.label_studio_password is None):
+        logger.error("configuration for label studio user and password are "\
+                    "required for modes other than delete, delete-all and delete-all-ls-data.")
+        raise ValueError("--label-studio-user and/or --label-studio-password missing")
+
+    if params.label_studio_default_storage_name != "" and \
+    params.label_studio_default_project_name ==  "": 
+        raise ArgumentError("Default storage cannot be set when default project is unset.")
 
     params.spec_id_created = params.spec_id is None
     if params.spec_id_created:
--- apps/label-studio/app/label_studio_init.sh
+++ apps/label-studio/app/label_studio_init.sh
@@ -1,4 +1,10 @@
 #label_studio_init.sh - run the initialization parts of the label studio image
 cd /label-studio
+# we may be running in a virtual evn, but the main image doens't install in one,
+# so we should exit.
+if [ "$VIRTUAL_ENV" != "" ]; then
+    unset VIRTUAL_ENV
+    export PATH=${PATH#*:}
+fi
 source /label-studio/deploy/docker-entrypoint.sh
 exec_entrypoint "$ENTRYPOINT_PATH/app/"
