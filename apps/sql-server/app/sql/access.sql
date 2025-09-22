cd5e96b91ffa02c9f67cb92f73f2d07c418a04ec
Gavin Matthews <gavin@aperturedata.io>
2025-09-22 00:20:31 +0000

    checkpoint

--- apps/sql-server/app/sql/access.sql
+++ apps/sql-server/app/sql/access.sql
@@ -18,3 +18,28 @@ GRANT SELECT ON ALL TABLES IN SCHEMA connection TO aperturedb;
 GRANT SELECT ON ALL TABLES IN SCHEMA descriptor TO aperturedb;
 
 GRANT USAGE ON FOREIGN SERVER aperturedb TO aperturedb;
+
+-- Lock down public
+REVOKE CREATE ON SCHEMA public FROM PUBLIC;
+REVOKE CREATE ON SCHEMA public FROM aperturedb;
+
+-- Lock down dangerous PLs just in case
+DROP EXTENSION IF EXISTS plpythonu;
+DROP EXTENSION IF EXISTS plperl;
+
+-- Revoke untrusted languages if they exist
+DO $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM pg_language WHERE lanname = 'plpythonu') THEN
+        REVOKE USAGE ON LANGUAGE plpythonu FROM PUBLIC;
+    END IF;
+END;
+$$;
+
+DO $$
+BEGIN
+    IF EXISTS (SELECT 1 FROM pg_language WHERE lanname = 'plperl') THEN
+        REVOKE USAGE ON LANGUAGE plperl FROM PUBLIC;
+    END IF;
+END;
+$$;
