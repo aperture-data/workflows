a03a0440142ce31ceb1dc103540a93eb2c3e8322
Gautam <gautam@aperturedata.io>
2025-10-13 14:15:11 -0400

    do not use dynamic hostname

--- apps/rag/test.sh
+++ apps/rag/test.sh
@@ -3,7 +3,7 @@ set -x
 set -euo pipefail
 
 WORKFLOW="rag"
-export RUNNER_NAME=${RUNNER_NAME:-$(hostname)}
+export RUNNER_NAME=${RUNNER_NAME:-runner}
 
 # Get the directory this script is in
 export BIN_DIR=$(dirname "$(readlink -f "$0")")
--- base/docker/build.sh
+++ base/docker/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -e
-RUNNER_NAME=${RUNNER_NAME:-$(hostname)}
+RUNNER_NAME=${RUNNER_NAME:-runner}
 COMPOSE_PROJECT_NAME="${RUNNER_NAME}-workflows-base"
 cd $(dirname "$(readlink -f "$0")")
 source ../../.commonrc
