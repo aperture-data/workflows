e30744bb1acda8b06efb3dba6d413b47c60e15f8
ad-claw000 <claw000@aperturedata.io>
2026-05-19 12:36:01 +0000

    chore: list s3 buckets for debugging

--- apps/ingest-from-bucket/test.sh
+++ apps/ingest-from-bucket/test.sh
@@ -20,6 +20,7 @@ AWS_ACCESS_KEY_ID=$(jq -r .access_key <<< ${WF_INGEST_BUCKET_AWS_CREDS})
 AWS_SECRET_ACCESS_KEY=$(jq -r .secret_key <<< ${WF_INGEST_BUCKET_AWS_CREDS})
 set -x
 
+docker run --rm -e "AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID" -e "AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY" amazon/aws-cli s3 ls
 bash ../build.sh
 
 CHECKER_NAME="aperturedata-internal/workflow-ingest-from-bucket-checker"
