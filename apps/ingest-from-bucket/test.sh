f2f216f4a0ebd3e6d6d412b83c5fa8c5d914ef85
ad-claw000 <claw000@aperturedata.io>
2026-05-19 11:49:25 +0000

    fix: set use_ssl to false in APERTUREDB_JSON for ingest-from-bucket test

--- apps/ingest-from-bucket/test.sh
+++ apps/ingest-from-bucket/test.sh
@@ -72,7 +72,7 @@ common+=( -e "WF_INGEST_VIDEOS=True")
 common+=( -e "WF_INGEST_PDFS=True")
 common+=( -e "DB_HOST=${DB_NAME}" )
 common+=( -e "VERIFY_HOSTNAME=False" )
-common+=( -e "APERTUREDB_JSON={\"host\": \"${DB_NAME}\", \"port\": 55555, \"username\": \"admin\", \"password\": \"admin\", \"use_ssl\": true, \"verify_hostname\": false}" )
+common+=( -e "APERTUREDB_JSON={\"host\": \"${DB_NAME}\", \"port\": 55555, \"username\": \"admin\", \"password\": \"admin\", \"use_ssl\": false, \"verify_hostname\": false}" )
 common+=( --network ${NW_NAME} )
 
 checker_opts=()
